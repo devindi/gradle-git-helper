9eaed266212b83de2cace73e09d29f0ab3907e8e
Andrew Mihin <andrei.mihin@studiomobile.ru>
2016-01-26 03:44:12 +0300

    <master> Add methods to get version

--- src/main/groovy/com/devindi/gradle/helper/GitHelperPlugin.groovy
+++ src/main/groovy/com/devindi/gradle/helper/GitHelperPlugin.groovy
@@ -3,19 +3,28 @@ package com.devindi.gradle.helper
 import org.gradle.api.*
 import com.devindi.gradle.helper.task.*
 import com.devindi.gradle.helper.dsl.*
+import com.devindi.gradle.helper.tools.*
 
 class GitHelperPlugin implements Plugin<Project> {
 	void apply(Project project) {
-		println "Applying plugin"
 		project.extensions.create("git", GitHelperPluginExtension)
-		println "Extension added"
 		project.task("bumpVersionCode", type: BumpVersionTask)
 		project.task("setupGit", type: InitProjectTask)
 		project.task('insertHooks', type: InitProjectTask.InsertGitHooksTask)
 		project.task('tagBuild', type: TagBuildTask)
 		project.task('bumpVersionName', type: BumpVersionNameTask)
-		println "Task added"
-
 		project.tasks.setupGit.dependsOn project.tasks.insertHooks
+	}
+
+	static String getVersionName(Project project) {
+		File versionPropFile = project.file(project.git.versionFile)
+		Properties versionProps = PropertyTools.readProperties(versionPropFile)
+		return versionProps['VERSION_NAME']
+	}
+
+	static Integer getVersionCode(Project project) {
+		File versionPropFile = project.file(project.git.versionFile)
+		Properties versionProps = PropertyTools.readProperties(versionPropFile)
+		return versionProps['VERSION_CODE'].toInteger()
 	}
 }
